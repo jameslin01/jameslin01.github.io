17dd7c442be801158e50c710422452fab3dc2e74
James Lin <arigatojames@gmail.com>
2023-04-30 03:54:33 +0100

    1

--- main.js
+++
@@ -1,6 +0,0 @@
-// Header Background Change on scroll
-let header = document.querySelector('header')
-
-window.addEventListener('scroll', () =>{
-    header.classList.toggle('shadow', window.scrollY > 0);
-});
